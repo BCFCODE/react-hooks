60c76f2cd9900900950a2f86fe85d4ddff1b3a46
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-08-02 19:14:16 +0330

    2.3. Reusable Components - start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import NameTag from "./components/nameTag";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="name title">Names List</h1>
-        <NameTag>peter</NameTag>
-        <NameTag>john</NameTag>
-        <NameTag>jill</NameTag>
+        <h3 className="name">Peter</h3>
+        <h3 className="name">John</h3>
+        <h3 className="name">Jill</h3>
       </header>
     </div>
   );
