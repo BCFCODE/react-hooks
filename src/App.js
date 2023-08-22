6ec0c7395a782a128470b1f5ed3f317a2256e39d
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-08-22 19:23:29 +0330

    an error occured when I add placeholder and type props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import NameTag from "./components/nameTag";
+import Input from './components/Input'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Input placeholder="Email" type="text"></Input>
         <h1 className="name title">Names List</h1>
         <NameTag firstName="peter" lastName="peterson"></NameTag>
         <NameTag firstName="john" lastName="johnson"></NameTag>
---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const Input = ({placeholder, type}) => {
+  return <input placeholder={props.placeholder} type={props.type}></input>;
+};
+
+export default Input;
