1170903f811266784cc11c9fcc3e580cb6ed9729
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-08-28 19:42:23 +0330

    START 2.6. Destructring Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.scss";
-import NameTag from "./components/nameTag";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="name title">Names List</h1>
-        <NameTag firstName="peter" lastName="peterson"></NameTag>
-        <NameTag firstName="john" lastName="johnson"></NameTag>
-        <NameTag firstName="jill" lastName="jillion"></NameTag>
+    
       </header>
     </div>
   );
