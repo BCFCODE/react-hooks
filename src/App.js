5bc58cfee372a26e04e5b58eb76c33b60e853ba1
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-08-28 20:31:10 +0330

    END 2.8. Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+/* eslint-disable react/display-name */
 import React from "react";
 import "./App.scss";
 import NameTag from "./components/nameTag";
 
+const makeGreen = (BaseComponent) => (props) => {
+  const addGreen = {
+    style: {
+      color: "green",
+    },
+  };
+
+  const newProps = {
+    ...props,
+    ...addGreen,
+  };
+
+  return <BaseComponent {...newProps} />;
+};
+
+const removeInline = (BaseComponent) => (props) => {
+  const newProps = { ...props };
+  delete newProps.style;
+  return <BaseComponent {...newProps}></BaseComponent>;
+};
+const GreenNameTag = makeGreen(NameTag);
+const CleanNameTag = removeInline(NameTag);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="name title">Names List</h1>
-        <NameTag firstName="peter" lastName="peterson"></NameTag>
-        <NameTag firstName="john" lastName="johnson"></NameTag>
-        <NameTag firstName="jill" lastName="jillion"></NameTag>
+        <GreenNameTag firstName="peter" lastName="peterson"></GreenNameTag>
+        <CleanNameTag
+          style={{ color: "red" }}
+          firstName="john"
+          lastName="johnson"
+        ></CleanNameTag>
+        <NameTag
+          style={{ color: "red" }}
+          firstName="jill"
+          lastName="jillion"
+        ></NameTag>
         <NameTag></NameTag>
       </header>
     </div>
