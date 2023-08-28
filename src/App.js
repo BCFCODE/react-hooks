2f18caadf5ddab10a06a0d8f1b67c8716bddf7a7
Morteza Bakhshandeh <bcfcode@gmail.com>
2023-08-28 19:55:37 +0330

    2.7. Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.scss";
-import Input from "./components/Input";
-
+import NameTag from "./components/nameTag";
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Input placeholder="Email" type="text"></Input>
+        <h1 className="name title">Names List</h1>
+        <NameTag firstName="peter" lastName="peterson"></NameTag>
+        <NameTag firstName="john" lastName="johnson"></NameTag>
+        <NameTag firstName="jill" lastName="jillion"></NameTag>
       </header>
     </div>
   );
